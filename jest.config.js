56eef8e089616093b3fb0229a6f8996fb3eb86c3
tandina <94601074+tandina@users.noreply.github.com>
2023-11-16 11:41:10 +0100

    Add files via upload

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  moduleNameMapper: {
+    "@core/(.*)": "<rootDir>/src/app/core/$1",
+  },
+  preset: "jest-preset-angular",
+  setupFiles: ["web-animations-js"],
+  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+  bail: false,
+  verbose: false,
+  collectCoverage: false,
+  coverageDirectory: "./coverage/jest",
+  testPathIgnorePatterns: ["<rootDir>/node_modules/"],
+  coveragePathIgnorePatterns: ["<rootDir>/node_modules/"],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+    },
+  },
+  roots: ["<rootDir>"],
+  modulePaths: ["<rootDir>"],
+  moduleDirectories: ["node_modules"],
+};
